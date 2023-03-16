d88f6196054d7cf27ba3d77b4c2de01266d2fe27
Michael Deng <michaeldeng55@gmail.com>
2023-03-16 14:45:32 -0400

    background added

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,13 +1,20 @@
 import React from "react";
 import Header from "../Header/Header";
 import axios from "axios";
+import bgimage from "../../assets/images/backgroundBars.svg"
+
+
+import "./HomePage.scss"
 
 const HomePage = () => {
 
   return (
-    <>
+    <div className = "home">
       <Header />
-    </>
+      <div className = "home__background">
+        <img src = {bgimage} alt = "background-bars" className = "home__background--image"/>
+      </div>
+    </div>
   );
 };
 
