059efd049099ad8c63aa8f63706405c2a58f1e6e
Michael Deng <michaeldeng55@gmail.com>
2023-03-16 15:33:05 -0400

    saved route link in header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import logo from "../../assets/images/musicbox-logo.png"
+import React from "react";
+import logo from "../../assets/images/musicbox-logo.png";
 import "./Header.scss";
+import { Link } from "react-router-dom";
+import {useState} from "react"
 
 const Header = () => {
-    return (
-        <div className = "header">
-            <div className = "header__container">
-                <img src = {logo} alt = "logo" className = "header__logo"></img>
-            </div>
-        </div>
-    );
+  const [atSaved, setAtSaved] = useState(true)
+
+  return (
+    <div className="header">
+      <div className="header__container">
+        <div className="header__block">Ignore</div>
+        <Link to="/" className="header__block">
+          <img src={logo} alt="logo" className="header__logo"></img>
+        </Link>
+        <Link to="/saved" className = "header__block">
+          <h2 className= {atSaved ? 'header__link--active' : 'header__link'}>Saved</h2>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
