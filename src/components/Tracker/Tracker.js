58652e9be081229a1a5e87022caa561790fc1eb8
Michael Deng <michaeldeng55@gmail.com>
2023-03-17 12:06:46 -0400

    saved personal info as state

--- src/components/Tracker/Tracker.js
+++ src/components/Tracker/Tracker.js
@@ -6,6 +6,9 @@ const Tracker = ({ note }) => {
   const [isEnter, setIsEnter] = useState(false);
   const [noteArray, setNoteArray] = useState([]);
 
+  const [songName, setSongName] = useState("");
+  const [yourName, setYourName] = useState("");
+
   useEffect(() => {
     //handles initial value which is undefined
     if (!note) {
@@ -39,11 +42,15 @@ const Tracker = ({ note }) => {
       </div>
       <div className="tracker__CTA">
         <button className="tracker__play">Play</button>
-        <form className = "tracker__form">
-            <label className = "tracker__form--label">Song Name</label>
-            <input className = "tracker__form--input"></input>
-            <label className = "tracker__form--label">Your Name</label>
-            <input className = "tracker__form--input"></input>
+        <form className="tracker__form">
+          <label className="tracker__form--label" htmlFor="song-name">
+            Song Name
+          </label>
+          <input className="tracker__form--input" name="song-name" onChange={(e) => setSongName(e.target.value)} value = {songName}></input>
+          <label className="tracker__form--label" htmlFor="your-name">
+            Your Name
+          </label>
+          <input className="tracker__form--input" name="your-name" onChange={(e) => setYourName(e.target.value)} value = {yourName}></input>
           <button className="tracker__save">Save Song</button>
         </form>
       </div>
