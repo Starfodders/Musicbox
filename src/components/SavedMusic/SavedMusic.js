78c68aa53f126132009f6345ba53397a91f8bf13
Michael Deng <michaeldeng55@gmail.com>
2023-03-17 12:49:41 -0400

    render saved songs in /Saved page

--- src/components/SavedMusic/SavedMusic.js
+++ src/components/SavedMusic/SavedMusic.js
@@ -1,19 +1,43 @@
 import "./SavedMusic.scss";
+import axios from "axios";
+
+import { useEffect, useState } from "react";
 
 const SavedMusic = () => {
+  const [songArray, setSongArray] = useState([]);
+
+  useEffect(() => {
+    const getSaved = async () => {
+      try {
+        const response = await axios.get("http://localhost:8080/saved");
+        setSongArray(response.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getSaved();
+  }, []);
+
   return (
     <div className="saved">
       <div className="saved__container">
-        <div className="saved__block">
-          <div className="saved__block--left">
-            <p>Song Name</p>
-            <p>Created at (timestamp)</p>
-            <p>Created by (name)</p>
-          </div>
-          <div className="saved__block--right">
-            <button>Play</button>
-          </div>
-        </div>
+        {songArray.map((song) => {
+          return (
+            <div className="saved__block">
+              <div className="saved__block--left">
+                <p className = "saved__block--text"><span className = "saved-labels">Name:</span> {song.songName}</p>
+                <p className = "saved__block--text"><span className = "saved-labels">Artist:</span> {song.yourName}</p>
+                <p className = "saved__block--text"><span className = "saved-labels">Created at:</span> {song.timestamp}</p>
+              </div>
+              <div className="saved__block--center">
+                <p>{song.songFile}</p>
+              </div>
+              <div className="saved__block--right">
+                <button className = "saved__block--button">Play</button>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
