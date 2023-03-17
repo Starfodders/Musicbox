8256514fcea5e1ca9d475ab3e748fe1054dec95e
Paul Kawabe <paul@MacBook-Pro-5.localdomain>
2023-03-17 11:15:08 -0400

    sequencer demo

--- src/components/TestPage/TestPage.js
+++ src/components/TestPage/TestPage.js
@@ -1,10 +1,20 @@
 import React, { useEffect, useState } from "react";
 import * as Tone from "tone";
+import Button from "../Button/Button";
 
 const SequencerExample = () => {
 
     //sequencer run state
     const [isRunning, setIsRunning] = useState(false);
+
+    //activeChordArray
+    const [activeChords, setActiveChords] = useState([]);
+    const [chordToAdd, setChordToAdd] = useState([]);
+
+    const addChordClickHandler = (note) => {
+        setChordToAdd(note)
+        console.log(chordToAdd)
+    }
 
     //chord variables
     const notes = {
@@ -22,17 +32,25 @@ const SequencerExample = () => {
       11: 'B'
     }
 
+    const chordQuality = {
+        0: 'M',
+        1: 'm',
+        2: 'M7',
+        3: 'm7',
+        4: 'v7'
+    }
+
     const generateChords = () => {
 
         const chordObj = {}
         
         for (let note in notes) {
           note = Number(note)
-          chordObj[notes[note]] = [notes[note]+'3', notes[(note + 7) % 12]+'4', notes[(note + 4) % 12]+'5', notes[note]+'5']
-          chordObj[notes[note] + 'm'] = [notes[note]+'3', notes[(note + 7) % 12]+'4', notes[(note + 3) % 12]+'5', notes[note]+'5']
-          chordObj[notes[note] + 'M7'] = [notes[note]+'3', notes[(note + 7) % 12]+'4', notes[(note + 4) % 12]+'5', notes[(note + 11) % 12]+'5']
-          chordObj[notes[note] + 'm7'] = [notes[note]+'3', notes[(note + 7) % 12]+'4', notes[(note + 3) % 12]+'5', notes[(note + 10) % 12]+'5']
-          chordObj[notes[note] + 'v7'] = [notes[note]+'3', notes[(note + 7) % 12]+'4', notes[(note + 4) % 12]+'5', notes[(note + 10) % 12]+'5']
+          chordObj[note + Object.keys(chordQuality)[0]] = [notes[note]+'3', notes[(note + 7) % 12]+'4', notes[(note + 4) % 12]+'5', notes[note]+'5']
+          chordObj[note + Object.keys(chordQuality)[1]] = [notes[note]+'3', notes[(note + 7) % 12]+'4', notes[(note + 3) % 12]+'5', notes[note]+'5']
+          chordObj[note + Object.keys(chordQuality)[2]] = [notes[note]+'3', notes[(note + 7) % 12]+'4', notes[(note + 4) % 12]+'5', notes[(note + 11) % 12]+'5']
+          chordObj[note + Object.keys(chordQuality)[3]] = [notes[note]+'3', notes[(note + 7) % 12]+'4', notes[(note + 3) % 12]+'5', notes[(note + 10) % 12]+'5']
+          chordObj[note + Object.keys(chordQuality)[4]] = [notes[note]+'3', notes[(note + 7) % 12]+'4', notes[(note + 4) % 12]+'5', notes[(note + 10) % 12]+'5']
         }
       
         return chordObj
@@ -41,14 +59,26 @@ const SequencerExample = () => {
       
     const chords = generateChords()
 
-    const ABass = ["A2", "E2"]
-    const BBass = ["B2", "F#2"];
-    const EBass = ["E2", "B2"]
-    const BbBass = ["Bb2", "F2"];
+    //bass patterns
+
+    const generateBass = () => {
+
+        const bassObj = {}
+
+        for (let note in notes) {
+            note = Number(note)
+            bassObj[note] = [notes[note]+'2', notes[(note + 7) % 12]+'2']
+        }
+
+        return bassObj
+
+    }
+
+    const bassPatterns = generateBass();
 
     //states for sequencer notes
     const [bassNote, setBassNote] = useState(["A2", "E2"]);
-    const [chord, setChord] = useState(chords.AM7);
+    const [chord, setChord] = useState(chords[92]);
 
     //callback functions for changing notes
     const changeSounds = (arp, bass) => {
@@ -102,22 +132,22 @@ const SequencerExample = () => {
     };
 
     //useEffect for bass
-    // useEffect(() => {
-    //     //sequence that plays bass note
-    //     const bassSequence = new Tone.Sequence((time, note) => {
-    //         bassSynth.triggerAttackRelease(note, "2n", time)
-    //     }, bassNote, '2n').start(0);
+    useEffect(() => {
+        //sequence that plays bass note
+        const bassSequence = new Tone.Sequence((time, note) => {
+            bassSynth.triggerAttackRelease(note, "2n", time)
+        }, bassNote, '2n').start(0);
 
-    //     // Set the loop points
-    //     Tone.Transport.loopStart = 0;
-    //     Tone.Transport.loopEnd = "1m";
-    //     Tone.Transport.loop = true;
+        // Set the loop points
+        Tone.Transport.loopStart = 0;
+        Tone.Transport.loopEnd = "1m";
+        Tone.Transport.loop = true;
 
-    //     return () => {
-    //         bassSequence.dispose();
-    //     };
+        return () => {
+            bassSequence.dispose();
+        };
 
-    // }, [bassNote])
+    }, [bassNote])
 
 
     //useEffect for the arpegiator
@@ -153,28 +183,19 @@ const SequencerExample = () => {
         }
     };
 
-  const buttonLabel = isRunning ? "Stop Sequencer" : "Start Sequencer";
-
-  //button generator 
-  const buttonGenerator = () => {
-
-    const buttons = []
-
-    for (let note in notes) {
-        buttons.push(<button onClick={setChord()}>{notes[note]}</button>)
-    }
-
-    return buttons
-  }
+  const startStopButton = isRunning ? "Stop Sequencer" : "Start Sequencer";
 
   return (
     <>
     <div className="test">
-        {buttonGenerator()}
+        <button onClick={handleToggleSequencer}>{startStopButton}</button>
+        <button onClick={() => {changeSounds(chords[22], bassPatterns[2])}}>{notes[2]+chordQuality[2]}</button>
+        <button onClick={() => {changeSounds(chords[44], bassPatterns[4])}}>{notes[4]+chordQuality[4]}</button>
+        <button onClick={() => {changeSounds(chords[92], bassPatterns[9])}}>{notes[9]+chordQuality[2]}</button>
     </div>
         <div>
         <h4>arpeggio parameters</h4>
-        //make inputs components probably
+        {/*make inputs components probably*/}
         <input 
           type="range" 
           min="0" 
@@ -194,6 +215,14 @@ const SequencerExample = () => {
         />
         <p>Release: {synthRelease}</p>
     </div>
+    {Object.keys(notes).map((note, i) => {
+        return (
+            <Button addChordClickHandler={addChordClickHandler} note={note} displayNote={notes[note]} key={i}/>
+        )
+    })}
+    {/* <form>
+        <Button setChordToAdd={setChordToAdd} note={note}/>
+    </form> */}
     </>
   );
 };
